e5158f43efb6ee541791990dbb39a8f667c56079
aarontark <tarkington.aaron@gmail.com>
2024-05-08 02:58:50 -0500

    added media queries

---
+++ app.js
@@ -0,0 +1,9 @@
+const heroImg = document.querySelector('.hero-img');
+
+console.log(heroImg);
+
+if (window.innerWidth >= 900) {
+    heroImg.src = './images/hero-desktop.jpg';
+}
+
+console.log(1);
